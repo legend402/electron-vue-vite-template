771d3df78678069e5e85e931151a623970e9e196
hanyj <1689699478@qq.com>
2023-02-09 21:16:27 +0800

    feat: 新增morse code页面

---
+++ src/pages/Morse/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="morse-template" flex="~ wrap" justify-center>
+      <div class="morse-item w-20% lg:w-10% md:w-15% m-[-0.5px]" v-for="code, text in MorseMap" flex leading-8 border="~ black solid">
+        <div class="morse-text" px-2 h-8>{{ text }}</div>
+        <div class="morse-code" flex-1 h-8 text-center>{{ code }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { MorseMap } from './static'
+
+defineOptions({
+  name: 'morse-摩斯密码'
+})
+</script>
+
+<style scoped>
+</style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,12 +1,12 @@
 import { type RouteRecordRaw } from 'vue-router';
 
-export const routes = handleRoute();
+export const routes = handleRoute()
 
 function handleRoute(): RouteRecordRaw[] {
   const result = import.meta.glob('../pages/**/*{.vue,.tsx}', { eager: true })
   return Object.keys(result).map(item => {
     const component = (result[item] as any).default
-    const [name, title] = component.name.split('-')
+    const [name, title] = component.name?.split('-') || []
     return {
       path: name === 'home' ? '/' : `/${name}`,
       name: name,
