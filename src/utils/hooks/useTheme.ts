d682fb8085281af2c1038bde37cc6d44aedcd0f4
hanyj <1689699478@qq.com>
2023-03-14 21:24:19 +0800

    feat: 添加全局皮肤切换

---
+++ src/utils/hooks/useTheme.ts
@@ -0,0 +1,28 @@
+import { useCssVar, useStorage } from "@vueuse/core"
+
+
+export const useTheme = () => {
+  
+  const colors = reactive([
+    'rgb(112,56,240)',
+    'rgb(49,181,247)',
+    'rgb(250,135,53)',
+    'rgb(249,197,21)',
+  ])
+  const storageColor = useStorage('main-theme', '')
+  console.log(storageColor.value);
+  
+  const color = useCssVar('--theme-color', document.body)
+  color.value = storageColor.value || colors[0]
+  
+  const changeStyle = (item: string) => {
+    color.value = item
+    storageColor.value = item
+  }
+
+  return {
+    changeStyle,
+    color,
+    colors
+  }
+}
